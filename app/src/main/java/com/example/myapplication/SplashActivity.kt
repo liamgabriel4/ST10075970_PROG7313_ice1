9056278a37ec0e89a31f2c16f6c8e3adfbb75799
liamgabriel4 <st10075970@vcconnect.edu.za>
2025-02-24 21:56:30 +0200

    addition of firebase, added functionalities however it is not accepting firebase as discussed in class and on teams

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var btnNext : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnNext = findViewById(R.id.btnNext)
+
+        btnNext.setOnClickListener{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
+
+    }
+}
